fb8458b78e519e83cc7eef4a07f6e3e68fc73fc7
Luis Alex Betancourt Santos <145590566+ALEXBETANCOUR@users.noreply.github.com>
2023-09-20 16:43:52 -0500

    Add files via upload
    termux.luis.betancourt

---
+++ sumaluisbetancourt.cpp
@@ -0,0 +1,17 @@
+/*programa:suma.cpp*/
+/*autor: luis betancourt*/
+/*fecha: 2023-09-14*/
+/*Descripcion: sumas dos numeros*/
+#include<iostream>
+using namespace std;
+int main()
+{
+        float x,y,z;
+        cout<<"Ingrese x:";cin>>x;
+                cout<<"Ingrese y:";cin>>y;
+        //Aqui se realiza la suma//
+        z=x+y;
+        cout<<"La suma de"<<x<<"+"<<y<<"="<<z<<endl;
+        return(0);
+}
+
